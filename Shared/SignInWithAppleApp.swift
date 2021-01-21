064315f20c456310a18345a23f6b32e350e392d1
maguowei <imaguowei@gmail.com>
2021-01-21 18:19:56 +0800

    Initial Commit

---
+++ Shared/SignInWithAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleApp.swift
+//  Shared
+//
+//  Created by maguowei on 2021/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
